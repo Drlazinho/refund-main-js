438654352440de07dc8ad7d642c186cb43a2dbcc
Lázaro Pimentel <lazbonfim@hotmail.com>
2024-07-28 22:28:37 -0300

    Add function update Totals

--- sripts.js
+++ sripts.js
@@ -4,6 +4,8 @@ const expense = document.querySelector('#expense')
 const category = document.querySelector('#category')
 
 const expenseList = document.querySelector('ul')
+const expensesQuantity = document.querySelector("aside header p span")
+const expensesTotal = document.querySelector("aside header h2")
 
 amount.oninput = () => {
     let value = amount.value.replace(/\D/g, "")
@@ -68,9 +70,52 @@ function expenseAdd(newExpense) {
         expenseInfo.append(expenseName, expenseCategory)
         expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
         expenseList.append(expenseItem)
+
+        updateTotals()
         
     } catch (error) {
         alert("Não foi possível atualizar a lista de despesas.")
         console.log(error)
+    }
+}
+
+function updateTotals() {
+    try {
+        const items = expenseList.children
+
+        expensesQuantity.textContent = `${items.length} ${items.length
+             > 1 ? "despesas" : "despesa"
+        }`
+
+        let total = 0
+
+        for(let item = 0; item < items.length; item++) {
+            const itemAmount  = items[item].querySelector(".expense-amount")
+
+            //remove no-numeric char e replace common with dot
+            let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",",".")
+
+            value = parseFloat(value)
+
+            if (isNaN(value)) {
+                return  alert(
+                    "Não foi possível calcular o total. O valor não parecer ser um número."
+                )
+            }
+
+            total += Number(value)
+        }
+
+        const symbolBRL = document.createElement("small")
+        symbolBRL.textContent = "R$"
+
+
+        total = formatCurrencyBRL(total).toUpperCase().replace("R$", "")
+
+        expensesTotal.innerHTML = ""
+        expensesTotal.append(symbolBRL, total)
+
+    } catch (error) {
+        alert("Não foi possível atualizar os totais.")
     }
 }
